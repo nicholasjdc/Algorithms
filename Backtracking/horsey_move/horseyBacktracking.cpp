7ddc161a1f3bb98e6cd7963f0416c856573f756b
Nicholas Denton-Cheng <68447872+nicholasjdc@users.noreply.github.com>
2022-06-08 22:20:19 -0700

    horsey

--- Backtracking/horsey_move/horseyBacktracking.cpp
+++ Backtracking/horsey_move/horseyBacktracking.cpp
@@ -21,6 +21,21 @@ int **boardCreation(int length){
     return board;
 
 }
+
+bool isValidMove(int ** board, pair<int, int> currentPosition, int length){
+    cout<<"START VALIDATION " << currentPosition.first << " ;" << currentPosition.second<<endl;
+    if(currentPosition.first <length && currentPosition.first >=0
+            && currentPosition.second <length && currentPosition.second >= 0
+            && board[currentPosition.first][currentPosition.second] == -1){
+                    cout<<"END VALIDATION TRUE"<<endl;
+
+                return true;
+    }else{
+            cout<<"END VALIDATION FALSE"<<endl;
+
+        return false;
+    }
+}
 void printBoard(int **board, int length){
     for(int x =0; x<length; x++){
         for(int y =0; y<length; y++){
@@ -29,9 +44,26 @@ void printBoard(int **board, int length){
             cout << endl;
     }
 }
-int traverseBoard(int **board, int length, vector<pair<int, int>> horseyMoves){
+int **traverseBoard(int **board, int length, int currMove, pair<int, int> currentPosition, vector<pair<int, int>> horseyMoves){
+    //Horsey makes a move
+    //if the move is valid, mark as such, otherwise ignore
+    //
+    for(pair<int, int> m: horseyMoves){
+        currentPosition.first += m.first;
+        currentPosition.second += m.second;
+        cout<<"AT VALIDATION"<<endl;
 
+        if(isValidMove(board, currentPosition, length)){
+            board[currentPosition.first][currentPosition.second] = currMove;
+            currMove +=1;
+            board = traverseBoard(board, length, currMove, currentPosition, horseyMoves);
+        
+        }else{}
+
+    }
+    return board;
 }
+
 int main(){
     int length;
     vector<pair<int, int>> horseyMoves = {{1,2},{1,-2}, {2,1}, 
@@ -40,6 +72,8 @@ int main(){
     cout << "input length of board: ";
     cin >> length;
     int **board = boardCreation(length);
+    printBoard(board, length);
+    board = traverseBoard(board, length, 0, {0,0}, horseyMoves);
     printBoard(board, length);
     return 0;
     
